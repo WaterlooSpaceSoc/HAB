d26915e70b40ce820cefceeb9415d43d1743a207
Chung Zheng <bczheng@uwaterloo.ca>
2016-02-29 19:31:06 -0500

    Changed the GUI framework from PySide to PyQt5

--- Operations/GroundControlGUI.py
+++ Operations/GroundControlGUI.py
@@ -1,9 +1,12 @@
 # Prototype window to be used for HAB team's communications platform
+#
+# Requires installing PyQt5 (I suggst using the .exe installer):
+# http://pyqt.sourceforge.net/Docs/PyQt5/installation.html
 
 import sys
-from PySide import QtGui
+from PyQt5 import QtGui, QtWidgets, QtCore
 
-class CommWindow (QtGui.QMainWindow):
+class CommWindow (QtWidgets.QMainWindow):
 
     def __init__(self):
         super (CommWindow, self).__init__()
@@ -11,27 +14,27 @@ class CommWindow (QtGui.QMainWindow):
 
     def initUI(self):               
         # Define menu bar actions
-        exitAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_exit.png'), 'Exit', self)
+        exitAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_exit.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(self.close)
         
-        connectAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_connect.png'), 'Connect', self)
+        connectAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_connect.png'), 'Connect', self)
         connectAction.setStatusTip('Connect to Ballon')
 
-        refreshAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_refresh.png'), 'Refresh', self)
+        refreshAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_refresh.png'), 'Refresh', self)
         refreshAction.setStatusTip('Refresh All (Connection, Statistics, Location)') 
         
-        locateAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_location.png'), 'Locate', self)
+        locateAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_location.png'), 'Locate', self)
         locateAction.setStatusTip('Show Balloon Location')
 
-        statsAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_stats.png'), 'Statistics', self)
+        statsAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_stats.png'), 'Statistics', self)
         statsAction.setStatusTip('Show Flight Statistics')
 
-        settingsAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_settings.png'), 'Settings', self)
+        settingsAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_settings.png'), 'Settings', self)
         settingsAction.setStatusTip('Edit Settings')
         
-        cutDownAction = QtGui.QAction(QtGui.QIcon('resHAB/glyphicons_fire.png'), 'Cut Down', self)
+        cutDownAction = QtWidgets.QAction(QtGui.QIcon('resHAB/glyphicons_fire.png'), 'Cut Down', self)
         cutDownAction.setStatusTip('Initiate Cut Down')
         
         # Add a menu bar
@@ -50,19 +53,19 @@ class CommWindow (QtGui.QMainWindow):
         self.toolbar.addAction(cutDownAction)
 
         # Create the command line interface
-        lblCommand = QtGui.QLabel('Command Line')
-        boxCommand = QtGui.QTextEdit()
-        lineCommand = QtGui.QLineEdit()
+        lblCommand = QtWidgets.QLabel('Command Line')
+        boxCommand = QtWidgets.QTextEdit()
+        lineCommand = QtWidgets.QLineEdit()
 
         # Create the grid layout and place all the widgets
-        grid = QtGui.QGridLayout()
+        grid = QtWidgets.QGridLayout()
         grid.setSpacing(10)
         grid.addWidget(lblCommand, 1, 0)
         grid.addWidget(boxCommand, 2, 0)
         grid.addWidget(lineCommand, 3, 0)
 
         # A QMainWindow contains the central widget, which contains the layout grid
-        centralWidget = QtGui.QWidget() 
+        centralWidget = QtWidgets.QWidget() 
         centralWidget.setLayout(grid)
         self.setCentralWidget(centralWidget)
         
@@ -78,6 +81,6 @@ class CommWindow (QtGui.QMainWindow):
 
 
 # Main Loop
-app = QtGui.QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 winMain = CommWindow()
 sys.exit(app.exec_())
---
+++ Operations/README.md
@@ -0,0 +1,6 @@
+# HAB
+University of Waterloo's High Altitude Ballooning Group
+
+Main window for the communications platform can be started by running GroundControl.py
+GUI is currently a work in progress and can be found in GroundControlGUI.py
+
